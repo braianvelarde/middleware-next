c201b10a6ae93544db3d0b37587d477530176006
Braian Velarde <braianvelarde@gmail.com>
2022-06-14 19:27:24 -0300

    test ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { GetServerSidePropsContext, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import requestIp from "request-ip";
 
-const Home: NextPage = () => {
+interface Props {
+  detectedIp: string;
+}
+
+const Home = ({ detectedIp }: Props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -18,7 +23,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -46,9 +51,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>{detectedIp}</p>
           </a>
         </div>
       </main>
@@ -59,14 +62,23 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const detectedIp = requestIp.getClientIp(ctx.req);
+  return {
+    props: {
+      detectedIp,
+    },
+  };
 }
 
-export default Home
+export default Home;
